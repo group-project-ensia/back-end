98b10bcfa3d1955d038579bc6f38b88f83b89799
Nesrine <nesrine.bouzid@ensia.edu.dz>
2025-04-19 22:45:19 +0100

    add to do endpoint

---
+++ src/routes/todo.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const todoController = require('../controllers/todoController.js');
+
+router.post('/', todoController.createTodo);
+router.get('/:userId', todoController.getTodosByUser);
+router.get('/:userId/done', todoController.getDoneTodos);
+router.get('/:userId/doing', todoController.getDoingTodos);
+router.put('/:id', todoController.updateTodo);
+router.delete('/:id', todoController.deleteTodo);
+
+module.exports = router;
